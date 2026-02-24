4fa88286c323e546a5a2a1d6454373cb8c1e2c2d
Shashank Lakhera <lakherashashank70@gmail.com>
2026-02-25 00:41:06 +0530

    piano UI created

---
+++ Musical-Piano/script.js
@@ -0,0 +1,58 @@
+let audio = new Audio()
+
+document.addEventListener('keydown',(e)=> {
+    // console.log(e.code);
+    if(e.code === 'Space')
+    {
+        audio.src = './sounds/51.mp3'
+        audio.play()
+    }
+    
+    if(e.code === 'KeyA')
+    {
+        audio.src = './sounds/52.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyS')
+    {
+        audio.src = './sounds/53.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyD')
+    {
+        audio.src = './sounds/54.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyF')
+    {
+        audio.src = './sounds/55.mp3'
+        audio.play();
+    }
+
+    if(e.code === 'KeyG')
+    {
+        audio.src = './sounds/56.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyH')
+    {
+        audio.src = './sounds/57.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyJ')
+    {
+        audio.src = './sounds/58.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyK' )
+    {
+        audio.src = './sounds/59.mp3'
+        audio.play();
+    }
+    if(e.code === 'KeyL')
+    {
+        audio.src = './sounds/60.mp3'
+        audio.play();
+    }
+    
+})
