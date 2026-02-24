0482c01404ff996473d9faa1f6f279272638e798
Shashank Lakhera <lakherashashank70@gmail.com>
2026-02-24 12:50:29 +0530

    build spotlight feature

--- spotlight/script.js
+++ spotlight/script.js
@@ -1,6 +1,15 @@
-const body = document.body;
+const spotlight = document.querySelector('.layer')
+const para = document.querySelector('p')
 
-body.addEventListener('mouseenter', (event) => {
-    console.log(event);
-    
-})
+document.addEventListener('mousemove', (event) => {
+    let x = event.clientX
+    let y = event.clientY
+
+    spotlight.style.background = `
+    radial-gradient(
+    circle at ${x}px ${y}px,
+    transparent 100px,
+    rgba(0,0,0,0.4) 200px,
+    rgba(0, 0, 0, 0.96) 300px
+  )`
+}) 
