d9ffbf94afe3622e70ab7c764212356d8b2d575a
Shashank Lakhera <lakherashashank70@gmail.com>
2026-03-03 21:58:13 +0530

    Improved UI and initiallized slack clone

--- Seven-Kingdom-Cast/script.js
+++ Seven-Kingdom-Cast/script.js
@@ -1,5 +1,7 @@
 import characters from './data.js'
 const container = document.querySelector('.container');
+const nav = document.querySelector('nav')
+const body = document.body;
 
 function openModal(character) {
 
@@ -14,10 +16,11 @@ function openModal(character) {
                         <img src="${character.image}" alt="">
                     </div>
                     <div class="modal-right">
-                        <h1>${character.characterName}</h1>
+                        <h1>${character.characterName} :&nbsp; <span>${character.realName}</span></h1>
                         <h3>${character.title}</h3>
                         <h4>${character.house}</h4>
                         <h5>Dialogue - ${character.dialogue}</h5>
+                        <h5>Status - ${character.status}</h5>
                         <hr>
                         <p class="desc">${character.description}</p>
                         <p id="quote">${character.quote}</p>
@@ -47,3 +50,15 @@ characters.forEach((ch) => {
     container.append(img)
 })
 
+body.addEventListener('wheel', (e) => {
+    
+    if(e.deltaY<0)
+    {
+        nav.style.display = "flex"
+        
+    }
+    else{
+        nav.style.display = "none"
+        
+    }
+})
