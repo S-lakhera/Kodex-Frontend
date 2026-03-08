e1a3fe0f77fd7729c46498e1801774ab540c2663
Shashank Lakhera <lakherashashank70@gmail.com>
2026-03-08 14:30:56 +0530

    Added js to improve UX

--- Slack/js/main.js
+++ Slack/js/main.js
@@ -6,6 +6,66 @@ const rDots = document.querySelectorAll('.r-dot')
 const img2 = document.querySelector('#img-2')
 const menuBar = document.querySelector('.menu-icon')
 const vNav = document.querySelector('.v-nav')
+const sourcesHeader = document.getElementById('sources-header');
+const sourcesContent = document.getElementById('sources-content');
+const sourcesIcon = document.getElementById('sources-icon');
+const modal = document.getElementById('join-modal')
+const modalBox = modal.querySelector('div')
+const closeModal = document.getElementById('close-modal')
+const ctaBtns = document.querySelectorAll('#join-btn')
+const navItems = document.querySelectorAll('.group.relative')
+
+
+navItems.forEach((item) => {
+    let menu = item.querySelector('.mega-menu')
+    let icon = item.querySelector('i')
+    
+    item.addEventListener('mouseenter', () => {
+        menu.classList.remove('opacity-0','invisible','translate-y-2')
+        
+        if(icon) icon.style.transform = 'rotate(180deg)'
+    })
+    item.addEventListener('mouseleave', () => {
+        menu.classList.add('opacity-0','invisible','translate-y-2')
+    })
+})
+
+
+const openModal = () => {
+    modal.classList.remove('opacity-0', 'pointer-events-none');
+    modal.classList.add('opacity-100')
+}
+
+const hideModal = () => {
+    modal.classList.remove('opacity-100')
+    modal.classList.add('opacity-0', 'pointer-events-none')
+}
+
+ctaBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        openModal()
+    })
+})
+
+closeModal.addEventListener('click', hideModal)
+modal.addEventListener('click', (e) => {
+    if(e.target === modal)
+    {
+        hideModal()
+    }
+})
+
+sourcesHeader.addEventListener('click', () =>{
+    const isOpen = sourcesContent.style.maxHeight !== '0px' && sourcesContent.style.maxHeight !== ''
+
+    if(isOpen){
+        sourcesContent.style.maxHeight = '0px'
+        sourcesIcon.style.transform = 'rotate(0deg)'
+    }else{
+        sourcesContent.style.maxHeight = sourcesContent.scrollHeight + 'px'
+        sourcesIcon.style.transform = 'rotate(180deg)'
+    }
+})
 
 menuBar.addEventListener('click', () => {
     
@@ -56,9 +116,11 @@ rDots.forEach((dot) => {
 
 
 body.addEventListener('wheel', (e) => {
-    if(e.deltaY>0)
+    console.log(window.pageYOffset);
+
+    if(e.deltaY>-0 && window.pageYOffset>340)
     {
-        header.style.transform = "translateY(-100%)"
+        header.style.transform = "translateY(-130%)"
     }
     else{
         header.style.transform = "translateY(0)"
